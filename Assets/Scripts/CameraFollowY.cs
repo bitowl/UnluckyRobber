9ce721a75f4de56299d824644d9d82edd3be081c
bitowl <github@bitowl.de>
2017-01-28 13:31:32 +0100

    Mount the victim

--- Assets/Scripts/CameraFollowY.cs
+++ Assets/Scripts/CameraFollowY.cs
@@ -13,6 +13,19 @@ public class CameraFollowY : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        Vector3 target = new Vector3(Player.position.x / 2, Player.position.y, this.gameObject.transform.position.z) + Offset;
+
+	    gameObject.transform.position = Smooth(gameObject.transform.position, target, 0.5f);
+
 		this.gameObject.transform.position = new Vector3 (Player.position.x / 2, Player.position.y, this.gameObject.transform.position.z) + Offset;
 	}
+
+    private Vector3 Smooth(Vector3 position, Vector3 target, float t)
+    {
+        return new Vector3(
+            Mathf.Lerp(position.x, target.x, t),
+            Mathf.Lerp(position.y, target.y, t),
+            Mathf.Lerp(position.z, target.z, t)
+        );
+    }
 }
---
+++ Assets/Scripts/MountVictim.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MountVictim : MonoBehaviour
+{
+
+    public Transform MountPoint;
+    public Transform Victim;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    Victim.transform.position = Vector3.zero;
+		Victim.SetParent(MountPoint, false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
