d8b855d72ec6a6e6e647da03ead23608b8b92efc
bitowl <github@bitowl.de>
2017-01-28 13:06:44 +0100

    asdf

---
+++ Assets/Scripts/CameraFollowY.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowY : MonoBehaviour {
+	public Transform Player;
+    public Vector3 Offset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.gameObject.transform.position = new Vector3 (Player.position.x / 2, Player.position.y + 1.5f, this.gameObject.transform.position.z);
+	}
+}
