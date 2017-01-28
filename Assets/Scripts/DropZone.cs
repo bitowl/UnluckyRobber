2184df57c02a7131a3d22700fa8233629868f976
bitowl <github@bitowl.de>
2017-01-28 16:53:08 +0100

    place to drop the victims to

---
+++ Assets/Scripts/DropZone.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropZone : MonoBehaviour {
+
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Victim")
+        {
+            Debug.Log("SCORE!");
+            Destroy(other.transform.parent.parent.gameObject);
+        }
+    }
+}
