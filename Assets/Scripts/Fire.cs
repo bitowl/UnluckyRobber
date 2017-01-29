143c77c606cd3421c587b1fe15497807fed6c9eb
bitowl <github@bitowl.de>
2017-01-29 15:02:49 +0100

    Correct fire with two players

--- Assets/Scripts/Fire.cs
+++ Assets/Scripts/Fire.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Fire : MonoBehaviour
 {
     private Player _inFire;
+    private Player _inFire2;
     private float inFireTime;
+    private float inFireTime2;
     public float TimeUltilDeath = 2;
 
 	// Use this for initialization
@@ -15,23 +17,48 @@ public class Fire : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () {
-	    if (_inFire != null)
+        if (_inFire2 != null)
+        {
+            inFireTime2 += Time.deltaTime;
+            if (inFireTime2 >= TimeUltilDeath)
+            {
+                _inFire2.Die("You burned to ashes");
+                _inFire2 = null;
+            }
+        }
+
+        if (_inFire != null)
 	    {
 	        inFireTime += Time.deltaTime;
 	        if (inFireTime >= TimeUltilDeath)
 	        {
 	            _inFire.Die("You burned to ashes");
 	            _inFire = null;
+	            if (_inFire2 != null)
+	            {
+	                _inFire = _inFire2;
+	                inFireTime = inFireTime2;
+	            }
 	        }
 	    }
+
 	}
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            _inFire = other.GetComponent<Player>();
-            _inFire.Burn();
-            inFireTime = 0;
+            if (_inFire == null)
+            {
+                _inFire = other.GetComponent<Player>();
+                _inFire.Burn();
+                inFireTime = 0;
+            }
+            else
+            {
+                _inFire2 = other.GetComponent<Player>();
+                _inFire2.Burn();
+                inFireTime2 = 0;
+            }
         }
     }
 
@@ -42,6 +69,15 @@ public class Fire : MonoBehaviour
             if (_inFire == other.GetComponent<Player>())
             {
                 _inFire = null;
+                if (_inFire2 != null)
+                {
+                    _inFire = _inFire2;
+                    inFireTime = inFireTime2;
+                }
+            }
+            if (_inFire2 == other.GetComponent<Player>())
+            {
+                _inFire2 = null;
             }
             Debug.Log("player " + other + " leaves fire.");
         }
